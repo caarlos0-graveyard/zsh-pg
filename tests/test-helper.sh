e697d93d72632c5ade17da12cb2759e822266850
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-01-27 12:14:53 -0200

    shellcheck fixes

--- tests/test-helper.sh
+++ tests/test-helper.sh
@@ -4,7 +4,9 @@ set -eo pipefail
 [[ ! -f "assert.sh" ]] && \
   wget https://raw.githubusercontent.com/lehmannro/assert.sh/master/assert.sh \
   &> /dev/null
+# shellcheck disable=1091
 source ./assert.sh
+# shellcheck disable=1091
 source ./pg.plugin.zsh
 
 _create-test-db-name() {
