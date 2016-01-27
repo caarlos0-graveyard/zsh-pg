367a8b0e36187608edaa60bea9f3533302bf6756
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-01-27 12:23:53 -0200

    fixed test helper

--- tests/test-helper.sh
+++ tests/test-helper.sh
@@ -10,8 +10,8 @@ source ./assert.sh
 source ./pg.plugin.zsh
 
 _create-test-db-name() {
-  if [ -f /usr/local/bin/gmd5sum ]; then
-    date | gmd5sum | cut -f1 -d' '
+  if test ! "$(which md5sum)"; then
+    date | md5 -r | cut -f1 -d' '
   else
     date | md5sum | cut -f1 -d' '
   fi
