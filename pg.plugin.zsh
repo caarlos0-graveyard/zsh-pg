5835e506a32779e8675a0060cfba99365617d77c
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-11-15 12:37:06 -0200

    fixed scripts

--- pg.plugin.zsh
+++ pg.plugin.zsh
@@ -1,26 +1,25 @@
 #!/bin/sh
 
-__pg-check() {
+__pg_check() {
   if [ "$#" != 2 ]; then
     echo "Usage: pg $1 <db_name>"
     return 1
   fi
 }
 
-__pg-check2() {
+__pg_check2() {
   if [ "$#" != 3 ]; then
     echo "Usage: pg $1 <origin> <target>"
     return 1
   fi
 }
 
-_pg-ls() {
+_pg_ls() {
   psql postgres -Atq -c "select d.datname from pg_catalog.pg_database d;"
 }
 
-_pg-kill-connections() {
-  local db_name="$*"
-  if __pg-check "kill-connections" "$db_name"; then
+_pg_kill_connections() {
+  if __pg_check "kill-connections" "$*"; then
     psql postgres > /dev/null <<EOF
     SELECT pg_terminate_backend(pid)
     FROM pg_stat_activity
@@ -30,45 +29,47 @@ EOF
   fi
 }
 
-_pg-create() {
+_pg_create() {
+  # shellcheck disable=SC2039
   local db_name="$*"
-  __pg-check "create" "$db_name" && \
+  __pg_check "create" "$db_name" && \
     createdb "$db_name"
 }
 
-_pg-drop() {
+_pg_drop() {
+  # shellcheck disable=SC2039
   local db_name="$*"
-  if __pg-check "drop" "$db_name"; then
-    _pg-kill-connections "$db_name"
+  if __pg_check "drop" "$db_name"; then
+    _pg_kill_connections "$db_name"
     dropdb --if-exists "$db_name"
   fi
 }
 
-_pg-cp() {
-  local origin="$1"
-  local target="$2"
-  if __pg-check2 "cp" "$origin" "$target"; then
-    _pg-kill-connections "$origin"
+_pg_cp() {
+  # shellcheck disable=SC2039
+  local origin="$1" target="$2"
+  if __pg_check2 "cp" "$origin" "$target"; then
+    _pg_kill_connections "$origin"
     psql postgres > /dev/null <<EOF
       CREATE DATABASE "$target" WITH TEMPLATE "$origin";
 EOF
   fi
 }
 
-_pg-mv() {
-  local origin="$1"
-  local target="$2"
-  if __pg-check2 "mv" "$origin" "$target"; then
-    _pg-kill-connections "$origin"
+_pg_mv() {
+  # shellcheck disable=SC2039
+  local origin="$1" target="$2"
+  if __pg_check2 "mv" "$origin" "$target"; then
+    _pg_kill_connections "$origin"
     psql postgres > /dev/null <<EOF
       ALTER DATABASE "$origin" RENAME TO "$target";
 EOF
   fi
 }
 
-_pg-dump-table() {
-  local db_name="$1"
-  local table_name="$2"
+_pg_dump_table() {
+  # shellcheck disable=SC2039
+  local db_name="$1" table_name="$2"
   if [ "$#" != 2 ]; then
     echo "Usage: pg dump-table <db_name> <table_name>"
   else
@@ -78,29 +79,30 @@ _pg-dump-table() {
 
 pg() {
   if [ "$#" != 0 ]; then
+    # shellcheck disable=SC2039
     local command="$1"; shift
   fi
   case "$command" in
     ls)
-      _pg-ls "$@"
+      _pg_ls "$@"
       ;;
     kill-connections)
-      _pg-kill-connections "$@"
+      _pg_kill_connections "$@"
       ;;
     create)
-      _pg-create "$@"
+      _pg_create "$@"
       ;;
     drop)
-      _pg-drop "$@"
+      _pg_drop "$@"
       ;;
     cp)
-      _pg-cp "$@"
+      _pg_cp "$@"
       ;;
     mv)
-      _pg-mv "$@"
+      _pg_mv "$@"
       ;;
     dump-table)
-      _pg-dump-table "$@"
+      _pg_dump_table "$@"
       ;;
     *)
       echo "Usage: pg (ls|kill-connections|create|drop|cp|mv|dump-table) <args>"
